ecdb4ff82572095b1feebdf6a53ef4d5591d1b9e
Pawel Kusz <pawel.kusz@student.put.poznan.pl>
2015-03-04 00:10:47 +0100

    Dodanie deklaracjie funkcji wraperow do wrapfunc.h
    Dodano deklaracje funkcji
    - Fgets
    - Fputs
    - Socket
    - Connect
    - Close
    - Dup2
    - Fork
    - Listen
    - Inet_pton
    - Inte_ntop
    - Signal

---
+++ wrapfunc.h
@@ -0,0 +1,18 @@
+#ifndef WRAP_FUNC
+#define WRAP_FUNC 
+
+#include "main.h"
+
+char	*Fgets(char *, int, FILE *);
+void	 Fputs(char *, int, FILE *);
+int		 Socket(int, int, int);
+void	 Connect(int, const SA *, socklen_t);
+void	 Close(int);
+void	 Dup2(int, int);
+pid_t	 Fork(void);
+void	 Listen(int, int);
+void	 Inet_pton(int, const char *, void *);
+const char		*Inet_ntop(int, const void *, char *, size_t);
+Sigfunc *Signal(int, Sigfunc *);
+
+#endif
