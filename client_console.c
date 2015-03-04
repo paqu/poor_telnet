0ca1e378118e1579859c696e94d4607dc3be1ea7
Pawel Kusz <pawel.kusz@student.put.poznan.pl>
2015-03-04 23:39:36 +0100

    Dodano inicjalizacje socketa na potrzeby windowsa

--- client_console.c
+++ client_console.c
@@ -1,3 +1,7 @@
+/*
+ * client_console application for WINDOWS system 
+ */
+
 #include "main.h"
 #include "func.h"
 #include "wrapfunc.h"
@@ -5,11 +9,21 @@
 
 int main(int argc, const char *argv[])
 {
-	int sockfd;
+	SOCKET sockfd;
+	WORD WRequiredVersion;
+	WSADATA WData;
+
 	struct sockaddr_in servaddr;
 	
 	if (argc != 3)
 		err_quit("usage: tcpcli <IPaddress> <Port>");
+
+	/* initialize winsock */
+	WRequiredVersion = MAKEWORD(2,0);
+	if (WSAStartup(WRequiredVersion, &WData) != 0) {
+		fprintf(stderr,"WSAStartup failed !");
+		exit(1);
+	}
 
 	sockfd = Socket(AF_INET, SOCK_STREAM,0);
 
@@ -23,5 +37,7 @@ int main(int argc, const char *argv[])
 	welcome_client(argv[1]);
 	client_console_handler(stdin,sockfd);
 
+	/* terminate use of the winsock */
+	WSACleanup();
 	exit(0);
 }
