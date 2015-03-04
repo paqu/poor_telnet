da62a6f49fb413aace092b781b31988ae32c36ea
Pawel Kusz <pawel.kusz@student.put.poznan.pl>
2015-03-04 23:50:19 +0100

    Dodanow winsock.h do naglowkow

--- main.h
+++ main.h
@@ -1,23 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H 
 
-#include	<sys/types.h>	/* basic system data types */
-#include	<sys/socket.h>	/* basic socket definitions */
-#include	<sys/time.h>	/* timeval{} for select() */
-#include	<time.h>		/* timespec{} for pselect() */
-#include	<netinet/in.h>	/* sockaddr_in{} and other Internet defns */
-#include	<arpa/inet.h>	/* inet(3) functions */
+#include	<winsock2.h>
+#include	<sys/time.h>	/* ??? timeval{} for select() */
+#include	<time.h>		/* ??? timespec{} for pselect() */
+#include	<arpa/inet.h>	/* ??? inet(3) functions */
 #include	<errno.h>
-#include	<fcntl.h>		/* for nonblocking */
-#include	<netdb.h>
 #include	<signal.h>
 #include	<stdio.h>
 #include	<stdlib.h>
 #include	<string.h>
-#include	<sys/stat.h>	/* for S_xxx file mode constants */
-#include	<unistd.h>
-#include	<sys/wait.h>
-#include	<sys/un.h>		/* for Unix domain sockets */
+#include	<unistd.h>	// ???
+#include	<sys/wait.h>// ???
 
 #define LISTENQ	1024
 #define MAXLINE 65536
